061ec46f9808a98b3aa4291eea7765715bd2cb14
Tenkouu <anhbayr.922@gmail.com>
2021-05-03 19:53:13 +0800

    Now possible to get value from input.

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
 // Дэлгэцтэй ажиллах контроллер
-var uiController = (function() {})();
+var uiController = (function() {
+
+    var DOMstrings = {
+        inputType: ".add__type",
+        inputDescription: ".add__description",
+        inputValue: ".add__value",
+        addBtn: ".add__btn",
+    }
+
+    return {
+        getInput: function(){
+            return {
+                type: document.querySelector(DOMstrings.inputType).value,
+                description: document.querySelector(DOMstrings.inputDescription).value,
+                value: document.querySelector(DOMstrings.inputValue).value,
+            };
+        },
+
+        getDOMstrings: function(){
+            return DOMstrings;
+        }
+    };
+})();
 
 // Санхүүтэй ажиллах контроллер
 var financeController = (function() {})();
 
 // Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
-var ctrlAddItem = function() {
-    // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
-    console.log("Дэлгэцээс өгөгдлөө авах хэсэг");
-    // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
-    // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
-    // 4. Төсвийг тооцоолно
-    // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
-};
 
-document.querySelector(".add__btn").addEventListener("click", function() {
-    ctrlAddItem();
-});
+    var DOM = uiController.getDOMstrings();
 
-document.addEventListener("keypress", function(event) {
-    if (event.keyCode === 13 || event.which === 13) {
-    ctrlAddItem();
-    }
-});
+    var ctrlAddItem = function() {
+        // 1. Оруулах өгөгдлийг дэлгэцээс олж авна.
+        console.log(uiController.getInput());
+        // 2. Олж авсан өгөгдлүүдээ санхүүгийн контроллерт дамжуулж тэнд хадгална.
+        // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
+        // 4. Төсвийг тооцоолно
+        // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
+    };
+
+    document.querySelector(DOM.addBtn).addEventListener("click", function() {
+        ctrlAddItem();
+    });
+
+    document.addEventListener("keypress", function(event) {
+        if (event.keyCode === 13 || event.which === 13) {
+        ctrlAddItem();
+        }
+    });
 })(uiController, financeController);
