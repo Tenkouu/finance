e4c6b3547b6121e600fe86b7edcf2f51676986ef
Tenkouu <anhbayr.922@gmail.com>
2021-05-07 13:11:23 +0800

    Made it so that you can save income and outcome.

--- app.js
+++ app.js
@@ -23,7 +23,51 @@ var uiController = (function() {
 })();
 
 // Санхүүтэй ажиллах контроллер
-var financeController = (function() {})();
+var financeController = (function() {
+    var Income = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var Expense = function(id, description, value) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+    var i1 = new Income(1, 'Цалин', 2500000);
+    var i2 = new Income(1, 'Сугалаа хожсон', 250000000);
+
+    // console.log(i1);
+    // console.log(i2);
+
+    var incomes = [];
+    var expenses = [];
+
+    incomes.push(i1);
+    incomes.push(i2);
+
+    // console.log(incomes);
+
+    console.log(incomes[1].value)
+
+    var data = {
+        allItems: {
+            inc: [],
+            exp: []
+        },
+
+        totals: {
+            inc: 0,
+            exp: 0
+        }
+    };
+    
+    data.allItems.inc.push(i1);
+
+    console.log(data.allItems.inc[0]);
+})();
 
 // Програмын холбогч контроллер
 var appController = (function(uiController, financeController) {
