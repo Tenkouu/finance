a48c38b2fb4d91c4e8ae1d1624cb0e24b33fdd50
Tenkouu <anhbayr.922@gmail.com>
2021-05-14 14:59:47 +0800

    Now possible to clear the field.

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ var uiController = (function() {
         inputType: ".add__type",
         inputDescription: ".add__description",
         inputValue: ".add__value",
-        addBtn: ".add__btn"
+        addBtn: ".add__btn",
+        incomeList: ".income__list",
+        expenseList: ".expenses__list"
     };
 
     return {
@@ -20,13 +22,29 @@ var uiController = (function() {
             return DOMstrings;
         },
 
+        clearFields: function() {
+            var fields = document.querySelectorAll(DOMstrings.inputDescription + ", " + DOMstrings.inputValue);
+            
+            // Convert List to Array
+            var fieldsArr = Array.prototype.slice.call(fields);
+        
+            fieldsArr.forEach(function(el, index, array) {
+                el.value = "";
+            });
+        
+            fieldsArr[0].focus();
+        
+            // for (var i = 0; i < fieldsArr.length; i++) {
+            //   fieldsArr[i].value = "";
+            // }
+        },
+
         addListItem: function(item, type){
             // Орлого, зарлагын элементийг агуулсан html-ийг бэлтгэнэ
-
             var html, list;
             if (type === "inc") {
-                list=".income__list";
-                html=`<div class="item clearfix" id="income-%id%">
+                list = DOMstrings.incomeList;
+                html = `<div class="item clearfix" id="income-%id%">
                         <div class="item__description">$$DESCRIPTION$$</div>
                         <div class="right clearfix">
                             <div class="item__value">$$VALUE$$</div>
@@ -36,8 +54,8 @@ var uiController = (function() {
                         </div>
                     </div>`;
             } else {
-                list=".expenses__list";
-                html=`<div class="item clearfix" id="expense-%id%">
+                list = DOMstrings.expenseList;
+                html = `<div class="item clearfix" id="expense-%id%">
                         <div class="item__description">$$DESCRIPTION$$</div>
                         <div class="right clearfix">
                             <div class="item__value">$$VALUE$$</div>
@@ -126,7 +144,8 @@ var appController = (function(uiController, financeController) {
 
         // 3. Олж авсан өгөгдлүүдээ вэб дээрээ тохирох хэсэгт нь гаргана
         uiController.addListItem(item, input.type);
-        
+        uiController.clearFields();
+
         // 4. Төсвийг тооцоолно
         // 5. Эцсийн үлдэгдэл, тооцоог дэлгэцэнд гаргана.
     };
